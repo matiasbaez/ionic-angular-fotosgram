b724a68dbeaae813f30c48dc4e2601b20a490ea8
Matías Báez <matiasbaez2512@gmail.com>
2021-04-10 12:24:44 -0400

    Updated, profile page

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { PostsService } from 'src/app/services/posts.service';
+import { UserService } from 'src/app/services/user.service';
+import { UIService } from 'src/app/services/ui.service';
+
+import { User } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +14,32 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  user: User = {};
+
+  constructor(
+    private postService: PostsService,
+    private userService: UserService,
+    private uiService: UIService,
+  ) {}
+
+  ngOnInit() {
+    this.user = this.userService.getUser();
+  }
+
+  async updateUser(fUpdate: NgForm) {
+    if (fUpdate.invalid) { return; }
+
+    const updated = await this.userService.updateUser(this.user);
+    if (updated) {
+      this.uiService.presentToast('Datos de perfil actualizado');
+    } else {
+      this.uiService.presentToast('No se puedo actualizar');
+    }
+  }
+
+  logout() {
+    this.userService.logout();
+    this.postService.postPage = 0;
+  }
 
 }
