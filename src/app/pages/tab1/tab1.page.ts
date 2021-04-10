388eedfb9a921690aa8704dbe7c2ca8158782092
Matías Báez <matiasbaez2512@gmail.com>
2021-04-10 11:55:06 -0400

    Updated, home page

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { PostsService } from 'src/app/services/posts.service';
+
+import { Post } from 'src/app/interfaces/interfaces';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +11,40 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public posts: Post[] = [];
+  public infScrollDisabled = false;
+
+  constructor(
+    private postsService: PostsService
+  ) {}
+
+  ngOnInit() {
+    this.nextPage();
+    this.postsService.newPost.subscribe(post => {
+      this.posts.unshift(post);
+    });
+  }
+
+  nextPage(event?, pull: boolean = false) {
+    this.postsService.getPosts(pull).subscribe(
+      (response) => {
+        this.posts.push(...response.posts);
+        if (event) {
+          event.target.complete();
+
+          if (response.posts.length === 0) { this.infScrollDisabled = true; }
+        }
+      },
+      (error) => {
+        console.log('Error: ', error);
+      }
+    );
+  }
+
+  refresh(event) {
+    this.nextPage(event, true);
+    this.posts = [];
+    this.infScrollDisabled = false;
+  }
 
 }
